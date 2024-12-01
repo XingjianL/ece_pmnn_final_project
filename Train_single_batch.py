3390f6830886086876e6896849f4b39db9fee14d
XingjianL <lixingjian2002@gmail.com>
2024-11-30 23:00:40 -0500

    rename to tanh model and added relu model

--- Train_single_batch.py
+++ Train_single_batch.py
@@ -2,13 +2,13 @@ from tools.data_loader import PMNNDataset, dynamic_collate_fn
 from torch.utils.data import DataLoader
 from functools import partial
 import matplotlib.pyplot as plt
-from model.sample_model import ODEFunc
+from model.relu_model import ODEFunc
 import torch.optim as optim
 import torch.nn as nn
 import torch
 from torchdiffeq import odeint_adjoint as odeint
 from tqdm import tqdm
-training_dataset = PMNNDataset(time_gap=7, batch_window=-1)
+training_dataset = PMNNDataset(time_gap=10, batch_window=-1)
 training_loader = DataLoader(
     training_dataset, 
     batch_size=1, # Note: this batch size is the number of files, more batches will be added by the window size and time_gap
@@ -23,7 +23,7 @@ func = ODEFunc().cuda()
 optimizer = optim.AdamW(func.parameters(), lr=1e-3)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.1)
 criterion = nn.MSELoss()
-losses = [0]*10
+avg_losses = [[]]*10
 fig = plt.figure(figsize=(8,2))
 # Iterate through the data
 for i in range(10):
@@ -40,9 +40,14 @@ for i in range(10):
         loss.backward()
         torch.nn.utils.clip_grad_norm_(func.parameters(), max_norm=1.0)
         optimizer.step()
-        if batch_n % 1000 == 0:
-            torch.save(func.state_dict(), f"model_{i}.pth")
-            tqdm.write(f"loss at {batch_n}: {loss.item()}. avg loss for epoch {i}: {losses[i]}")
+        batch_n += 1
+        if len(avg_losses[i]) == 0:
+            avg_losses[i].append(loss.item())
+        else:
+            avg_losses[i].append(avg_losses[i][-1]+(loss.item() - avg_losses[i][-1])/batch_n)
+        if batch_n % 200 == 1:
+            torch.save(func.state_dict(), f"relu_model_{i}.pth")
+            tqdm.write(f"loss at {batch_n}: {loss.item()}. avg loss for epoch {i}: {avg_losses[i][-1]}")
         if batch_n % 1 == 0:
             plt.clf()
             fig.suptitle(f"Epoch {i}. Batch {batch_n}")
@@ -56,16 +61,15 @@ for i in range(10):
             plt.plot(t,torch.squeeze(pred.cpu().detach())-y)
             plt.title("pred - GT")
             plt.subplot(144)
-            plt.plot(losses)
+            plt.plot(torch.tensor(avg_losses).T)
             plt.title("losses")
             plt.show(block=False)
             plt.pause(0.1)
-        batch_n += 1
-        losses[i] += (loss.item() - losses[i])/batch_n
+        
     scheduler.step()
-    print(losses)
-    print(f"Saving model as model_{i}.pth")
-    torch.save(func.state_dict(), f"model_{i}.pth")
+    #print(avg_losses)
+    print(f"Saving model as relu_model_{i}.pth")
+    torch.save(func.state_dict(), f"relu_model_{i}.pth")
 print('val')
 for batch in val_loader:
     t0, u0, x0, t,u,x = batch
---
+++ model/relu_model.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import torch
+class ODEFunc(nn.Module):
+    def __init__(self):
+        super(ODEFunc, self).__init__()
+        # Defining a two layer MLP with tanh activation
+        self.net = nn.Sequential(
+            nn.Linear(21, 100),   # modify from 2 input/output to 3 input/output
+            nn.ReLU(),
+            nn.Linear(100, 100),
+            nn.ReLU(),
+            nn.Linear(100, 100),
+            nn.ReLU(),
+            nn.Linear(100, 100),
+            nn.ReLU(),
+            nn.Linear(100, 100),
+            nn.ReLU(),
+            nn.Linear(100, 100),
+            nn.ReLU(),
+            nn.Linear(100, 13),
+        )
+        ## Providing a specific initialization of the weights and biases
+        for m in self.net.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+                nn.init.constant_(m.bias, val=0)
+
+    def forward(self, t, y):
+        #print(y.shape, t.shape,self.net(y).shape)
+        return torch.cat([torch.zeros((y.shape[0],8)).cuda(), self.net(y)], 1)
