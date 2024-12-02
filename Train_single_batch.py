9571af6c1aea6e3482117b35849b85ab23a3a168
XingjianL <lixingjian2002@gmail.com>
2024-12-01 21:50:14 -0500

    issues with unstable ode

--- Train_single_batch.py
+++ Train_single_batch.py
@@ -20,14 +20,15 @@ val_dataset = PMNNDataset(start_time=4, stop_time=5, time_gap=1)
 val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, collate_fn=partial(dynamic_collate_fn, window_size=-1))
 
 func = ODEFunc().cuda()
-func.load_state_dict(torch.load("relu_model_0.pth", weights_only=True))
-optimizer = optim.AdamW(func.parameters(), lr=1e-3)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.1)
+model_prefix = "ELU_model"
+#func.load_state_dict(torch.load("relu_model_1.pth", weights_only=True))
+optimizer = optim.AdamW(func.parameters(), lr=1e-4)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
 criterion = nn.MSELoss()
 avg_losses = [[]]*10
-fig = plt.figure(figsize=(8,2))
+fig = plt.figure(figsize=(12,3))
 # Iterate through the data
-for i in range(1,10):
+for i in range(1,4):
     print(f"--it {i}: total batches: {len(training_loader)}")
     batch_n = 0
     for batch in tqdm(training_loader):
@@ -37,40 +38,46 @@ for i in range(1,10):
         y0 = torch.cat([u0,x0],1)
         y = torch.cat([u,x],1)
         pred = odeint(func,y0.cuda(),t.cuda(),method="rk4")
-        loss = criterion(torch.squeeze(pred),y.cuda())
+        loss = criterion(torch.squeeze(pred)[8:],y[8:].cuda())
+        if torch.isnan(loss).any() or torch.isinf(loss).any() or loss.item() > 10000:
+            batch_n += 1
+            tqdm.write(f"odd loss detected in loss at batch {batch_n}: {loss.item()}")
+            continue
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(func.parameters(), max_norm=1.0)
+        torch.nn.utils.clip_grad_norm_(func.parameters(), max_norm=0.2)
         optimizer.step()
+
         batch_n += 1
         if len(avg_losses[i]) == 0:
             avg_losses[i].append(loss.item())
         else:
             avg_losses[i].append(avg_losses[i][-1]+(loss.item() - avg_losses[i][-1])/batch_n)
-        if batch_n % 200 == 1:
-            torch.save(func.state_dict(), f"relu_model_{i}.pth")
+        if batch_n % 200 == 0:
+            torch.save(func.state_dict(), f"{model_prefix}_{i}.pth")
             tqdm.write(f"loss at {batch_n}: {loss.item()}. avg loss for epoch {i}: {avg_losses[i][-1]}")
-        if batch_n % 1 == 0:
+        if batch_n % 100 == 0:
             plt.clf()
             fig.suptitle(f"Epoch {i}. Batch {batch_n}")
             plt.subplot(141)
-            plt.plot(t,torch.squeeze(pred.cpu().detach()))
+            plt.plot(t,torch.squeeze(pred.cpu().detach())[:,8:])
             plt.title("pred")
             plt.subplot(142)
-            plt.plot(t,y)
+            plt.plot(t,y[:,8:])
             plt.title("GT")
             plt.subplot(143)
             plt.plot(t,torch.squeeze(pred.cpu().detach())-y)
             plt.title("pred - GT")
             plt.subplot(144)
             plt.plot(torch.tensor(avg_losses).T)
-            plt.title("losses")
+            plt.title("avg losses")
+            plt.ylim([0,1])
             plt.show(block=False)
             plt.pause(0.1)
         
     scheduler.step()
     #print(avg_losses)
-    print(f"Saving model as relu_model_{i}.pth")
-    torch.save(func.state_dict(), f"relu_model_{i}.pth")
+    print(f"Saving model as {model_prefix}_{i}.pth")
+    torch.save(func.state_dict(), f"{model_prefix}_{i}.pth")
 print('val')
 for batch in val_loader:
     t0, u0, x0, t,u,x = batch
